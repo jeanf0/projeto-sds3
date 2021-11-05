494fdd3447adf2d66650fb396f9fd2d5254683c7
Jean Fraga <jeanfraga11@gmail.com>
2021-11-05 18:37:48 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -12,7 +12,7 @@ import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.service.SellerService;
 
 @RestController
-@RequestMapping(value = "/seller")
+@RequestMapping(value = "/sellers")
 public class SellerController {
 	
 	@Autowired
